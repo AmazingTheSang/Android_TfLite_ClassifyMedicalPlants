e2e746f20585e339bfb86153d3a58691a283869e
prd-sang-pham <sang.pham@paradox.ai>
2023-11-23 19:12:49 +0700

    show image

--- app/src/main/java/com/example/android_tflite_classifymedicalplants/Adapter/PlantAdapter.kt
+++ app/src/main/java/com/example/android_tflite_classifymedicalplants/Adapter/PlantAdapter.kt
@@ -9,23 +9,28 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.example.android_tflite_classifymedicalplants.Model.PlantModel
 import com.example.android_tflite_classifymedicalplants.R
+import com.squareup.picasso.Picasso
 
 class PlantAdapter(context: Context, plants: MutableList<String?>) :
-    ArrayAdapter<String?>(context, 0, plants ) {
+    ArrayAdapter<String?>(context, 0, plants) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        var listitemView = convertView
-        if (listitemView == null) {
+        var listItemView = convertView
+        if (listItemView == null) {
             // Layout Inflater inflates each item to be displayed in GridView.
-            listitemView = LayoutInflater.from(context).inflate(R.layout.item_plant, parent, false)
+            listItemView = LayoutInflater.from(context).inflate(R.layout.item_plant, parent, false)
         }
 
         val imgUrl: String? = getItem(position)
-        val courseIV = listitemView?.findViewById<ImageView>(R.id.plantImgv)
+        val courseIV = listItemView?.findViewById<ImageView>(R.id.plantImgv)
 
-        courseIV?.setImageResource(R.drawable.ic_launcher_foreground)
-        return listitemView!!
+        if (imgUrl?.isEmpty() == true) {
+            courseIV?.setImageResource(R.drawable.ic_launcher_foreground)
+        } else {
+            Picasso.get().load(imgUrl).resize(100, 100).into(courseIV)
+        }
+
+        return listItemView!!
     }
 }
 
--- app/src/main/java/com/example/android_tflite_classifymedicalplants/ModelHelper/ModelHelper.kt
+++ app/src/main/java/com/example/android_tflite_classifymedicalplants/ModelHelper/ModelHelper.kt
@@ -42,8 +42,17 @@ class ModelHelper(private val context: Context, private val imageSize: Int) {
             return outputFeature0.floatArray
         }
 
-        fun getPredictModels(plantModels: List<PlantModel>, confidences: FloatArray): List<PredictModel> {
-            val predictModels = plantModels.map { PredictModel(label = it.name, description = it.des, listImgUrl = it.listImgUrl) }
+        fun getPredictModels(
+            plantModels: List<PlantModel>,
+            confidences: FloatArray
+        ): List<PredictModel> {
+            val predictModels = plantModels.map {
+                PredictModel(
+                    label = it.name,
+                    description = it.des,
+                    listImgUrl = it.listImgUrl
+                )
+            }
             predictModels.withIndex().onEach { (index, model) ->
                 if (confidences.indices.contains(index)) {
                     model.confidence = confidences[index]
@@ -56,18 +65,71 @@ class ModelHelper(private val context: Context, private val imageSize: Int) {
             return listOf(
                 PlantModel(
                     "Bạc hà",
-                    " Bạc hà là một loại rau gia vị, đồng thời là một vị thuốc rất phổ biến dùng chữa cảm sốt, cảm mạo, nhức đầu, kích thích tiêu hóa, giảm đau",
+                    "Bạc hà là một loại cây thân thảo, cao khoảng 60-80cm, có tên khoa học là Mentha Arvensis, tên tiếng Anh là mint. Cây bạc hà thân hình vuông, màu xanh hoặc tím nhạt.",
                     arrayListOf(
-
+                        "", "", ""
                     )
                 ),
-                PlantModel("Bồ đề", "Cây bồ đề là một loại cây có nguồn gốc từ Ấn Độ, là loại cây thiêng liêng gắn liền với Phật pháp. Đây là loại cây cảnh sân vườn được trồng phổ biến ở Việt Nam."),
-                PlantModel("Cà ri", "Cây cà ri còn gọi là curry hay Murraya koenigii là một loại cây có nguồn gốc từ Ấn Độ. Lá của loại cây này không chỉ có hương thơm đặc trưng mà còn có những công dụng thần kỳ đối với sức khỏe của con người."),
-                PlantModel("Cải bẹ xanh", ""),
-                PlantModel("Chanh", ""),
-                PlantModel("Chùm ngây", ""),
-                PlantModel("Cỏ cà ri", ""),
-                PlantModel("Dâm bụt", ""),
+                PlantModel(
+                    "Bạch đồng nữ",
+                    " Bạch đồng nữ hay còn gọi là cây mò trắng, cây bấn trắng. Tên khoa học là Clerodendrum fragrans Vent, thuộc họ cỏ roi ngựa, có pháp danh khoa học là Verbenaceae.",
+                    arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Cam thảo đất",
+                    "Cam thảo đất hay cam thảo nam có tên khoa học là Seoparia Dulcis L, là loại cây thân thảo, mọc thẳng, cao 30 - 80cm, thân tròn, thuộc họ thân thảo, thân mềm, rễ hình trụ to.",
+                    arrayListOf(
+                        "", "", ""
+                    )
+                ),
+
+                PlantModel(
+                    "Cỏ mần trầu", "Cỏ mần trầu còn được gọi là Thanh tâm thảo, Ngưu cân thảo, Cỏ chì tía, Cỏ vườn trầu, có tên khoa học là Eleusine indica (L.) Gaertn – thuộc họ Lúa (Poaceae).", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Cúc tần", "Cúc tần hay cây từ bi, lức, lức ấn là loài thực vật có hoa thuộc họ Cúc. Cây cúc tần chứa hợp chất β-sitosterol và stigmasterol, là những chất có tác dụng chữa bệnh đái tháo đường.", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Diệp hạ châu", "Cây chó đẻ răng cưa hay còn gọi là cây chó đẻ, diệp hạ châu, diệp hạ châu đắng, cây cau trời. Tên Hán Việt khác: trân châu thảo, nhật khai dạ bế, diệp hậu châu. Cây có hạt tròn xếp thành hàng dưới lá nên gọi là diệp hạ châu.", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Hạ khô thảo", "Hạ khô thảo với tên khoa học đầy đủ là Spira Prunella Vulgaris, thuộc họ Labiatae", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Hoắc hương", "Hoắc hương là vị thuốc quý được dùng nhiều trong Đông y", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Húng chanh", "Húng chanh hay tần dày lá, rau thơm lùn, rau thơm lông, rau tần là cây thuộc họ Hoa môi", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Hy thiêm", "Hy thiêm, hay còn có tên dân gian khác là cỏ đĩ, cứt lợn, hy kiểm thảo, hy tiên, niêm hồ thái, chư cao, hổ cao, chó đẻ, nụ áo rìa..., là một loài thực vật thuộc họ Cúc", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Kinh giới", "Kinh giới, kinh giới rìa hay kinh giới trồng là loài cây thảo thuộc họ Hoa môi, là một loại rau thơm và cây thuốc", arrayListOf(
+                        "", "", ""
+                    )
+                ),
+                PlantModel(
+                    "Sâm ngọc linh", "Sâm Ngọc Linh là một loài cây thuộc Họ Cuồng cuồng, còn gọi là sâm Việt Nam", arrayListOf(
+                        "", "", ""
+                    )
+                ),
             )
         }
 
